956394d6de07c80b4a55b614ec8bdb39b64e2196
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2023-07-22 16:47:21 -0500

    Add files via upload

---
+++ 168.py
@@ -0,0 +1,6 @@
+def __init__(self, name, author,price,publishing_year):
+	self.book_name = nameprint("Book Name: "+self.book_name)
+	years_ago_published = 2020 - self.book_publishing_year
+	Book1 = Bookshelf("Harry Potter and the Philosophr's Stone","J. K. Rowling",1928,1997)
+	Book1.add_book()
+	Book1.years_since_published()
